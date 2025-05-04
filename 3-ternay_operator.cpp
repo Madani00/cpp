de142167bbd68cf3a52e7d0a66fc923c093e8b36
Madani00 <mamchart@gmail.com>
2025-05-04 15:32:15 +0100

    well done

---
+++ 3-ternay_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// ternary operator ?: a replacement to if statements
+
+int main()
+{
+    int age = 20;
+
+    age < 30 ? std::cout << "you passed \n" : std::cout << "game over \n";
+}
