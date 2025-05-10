afde9fd3ab968901cb1267726d2efe7478d2f840
Madani00 <mamchart@gmail.com>
2025-05-10 12:03:21 +0100

    well done

--- 5-array.cpp
+++ 5-array.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
-// type conversion
+
 int main()
 {
-    std::string foods[4] = {"couscous", "pizza", "tagine", "meal", };
+    std::string foods[20] = {"couscous", "pizza", "tagine", "meal", };
+    
+    // to fill and array of elements
+    fill(foods, foods + 20, "pizza");
+
     for(std::string f : foods){
         std::cout << f << '\n';
     }
